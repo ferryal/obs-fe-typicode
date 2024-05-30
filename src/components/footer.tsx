ba15a781a0f91b1037a9dd5eb6a462f1c90668e9
Ferryal Fajar <ferryal21@gmail.com>
2024-05-30 23:28:58 +0700

    [feat] add layout & footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 opacity-80 left-5">
+      <small className="block mb-2 text-sm">Ferryal Fajar &copy; 2024 OBS</small>
+    </footer>
+  );
+};
+
+export default Footer;
